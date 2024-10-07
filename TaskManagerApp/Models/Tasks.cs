7a3d44653f6366e6201ad4b9fea6ad96508d1f3f
malshi27 <malshiamanda27@gmail.com>
2024-10-07 22:08:31 +0530

    Add project files.

---
+++ TaskManagerApp/Models/Tasks.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerApp.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
